fe3fdf65f030d5190cad6052efea65a3687a3e7d
o.bereznoy <oleg.bereznoy@retal.dp.ua>
2022-05-24 01:17:25 +0200

    HWcheck

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,67 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String[] words = new String[] {"gin","zen","gig","msg"};
+        String[] words1 = new String[] {"gin","zen","gig","msg"};
         System.out.println("Input: words = [\"gin\",\"zen\",\"gig\",\"msg\"]");
-        System.out.println("Output: "+ ex1(words));
+        System.out.println("Output: "+ ex1(words1));
+        String[] words2 = new String[] {"a"};
+        System.out.println("Input: words = [\"a\"]");
+        System.out.println("Output: "+ ex1(words2));
+        System.out.println();
+
+        System.out.println("Input: 19, Output: "+ex2(19));
+        System.out.println("Input: 2, Output: "+ex2(2));
 
     }
 
     public static int ex1(String[] strs){
+        List mSet = new LinkedList();
+        String[] mcode = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...",  "-", "..-", "...-",".--", "-..-", "-.--","--.." };
         char ch;
-        String morse = "";
-        Set morseSet = new HashSet();
-        final String[] morsecode = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
-                ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...",  "-", "..-", "...-",
-                ".--", "-..-", "-.--","--.." };
-        int i;
-        for (i = 0; i < strs.length; i++) {
+        String m = "";
+        for (int i = 0; i < strs.length; i++) {
             for (int j = 0; j < strs[i].length(); j++) {
                 ch = strs[i].charAt(j);
-                morse += morsecode[ch - 'a'];
+                m = m + mcode[ch - 97];
             }
-            morseSet.add(morse);
-            morse = "";
+            mSet.add(m);
+            m = "";
         }
-        return morseSet.size();
+        Map result =
+                (Map) mSet.stream().collect(
+                    Collectors.groupingBy(
+                            Function.identity()));
+        return result.size();
+    }
+
+    public static Boolean ex2(int inp) {
+        String inpS = "" + inp;
+        List mL = new LinkedList<Integer>();
+        for (int i = 0; i < inpS.length(); i++) {
+            int ss = inpS.charAt(i)-'0';
+            mL.add(ss);
+        }
+        Integer sum = 0;
+        for (int i = 0; i < mL.size();  i++) {
+            int zp = (int) mL.get(i);
+            sum = sum + zp * zp;
+        }
+        if (sum == 1) {
+            return true;
+        } else {
+
+            try {
+               if (ex2(sum)==true) {return true;};
+            } catch (StackOverflowError  e) {
+                return false;
+            }
+        }
+        return false;
     }
 }
+
